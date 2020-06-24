01886798e65e158f067a01a11e8a10dbb6a18f0f
egimoro <gimoremma@gmail.com>
2020-06-24 01:16:26 +0100

    Attempted Javascript confirm function

--- melbournehseprice/static/javascript/funcs.js
+++ melbournehseprice/static/javascript/funcs.js
@@ -6,3 +6,14 @@ function AddHouse() {
 function AddLocation() {
   alert("Location Added")
 }
+
+function UpdateHouse() {
+  r = confirm("Are you sure you want to update {{ house }}?");
+  
+  if (r == true) {
+    alert(" Updated ");
+    } else {
+        alert("Not updated");
+      }
+  
+}
